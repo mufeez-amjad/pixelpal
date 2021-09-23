9e049f12d69aaeba04c110d8a046b1acc6e07391
Mufeez Amjad <mufeez.amjad@outlook.com>
2021-09-23 01:24:06 -0400

    add husky pre-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 
 const { app } = window.require('@electron/remote');
 
 function App() {
-  
 	const [path] = useState(app.getAppPath());
 
-	return (
-		<div>
-			Hello world from {path}!
-		</div>
-	);
+	return <div>Hello world from {path}!</div>;
 }
 
 export default App;
