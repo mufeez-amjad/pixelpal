b1b6d7fbff14d268c691d1a8142407aebc7ee20f
Damian Reiter <damian@Damians-MacBook-Pro.local>
2021-09-25 00:12:09 -0400

    Add basic form with slider finally working

--- src/ReminderForm.jsx
+++ src/ReminderForm.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-const { ipcRenderer } = window.require('electron');
+import { FormGroup, InputGroup, Slider } from '@blueprintjs/core';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+// const { ipcRenderer } = window.require('electron');
 
-function test() {
-	const habit = {
-		name: 'testHabit',
-		interval: 5,
-		days: 'MWF'
-	};
-	ipcRenderer.invoke('insertHabit', habit);
-	ipcRenderer.invoke('getHabits');
-}
+// function test() {
+// 	const habit = {
+// 		name: 'testHabit',
+// 		interval: 5,
+// 		days: 'MWF'
+// 	};
+// 	ipcRenderer.invoke('insertHabit', habit);
+// 	ipcRenderer.invoke('getHabits');
+// }
 
-function ReminderForm() {
-	return (
-		<div>
-			<Link to="/">
-				<button onClick={test}>Submit</button>
-			</Link>
-		</div>
-	);
+class ReminderForm extends React.Component {
+	constructor() {
+		super();
+		this.state = { interval: 5 };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	render() {
+		return (
+			<div>
+				<FormGroup
+					label="Add new Habit"
+					labelFor="text-input"
+					labelInfo="(required)"
+				>
+					<InputGroup id="text-input" placeholder="Drink water" />
+					<Slider
+						min={0}
+						max={10}
+						stepSize={0.1}
+						labelStepSize={10}
+						value={this.state.interval}
+						onChange={value => this.setState({ interval: value })}
+					/>
+				</FormGroup>
+			</div>
+		);
+	}
 }
 
 export default ReminderForm;
