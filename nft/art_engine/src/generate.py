1f310e93ebe4032acd7bca03eba2f7a445288194
MichaelxhJiang <michaelxhj@outlook.com>
2022-01-11 13:07:46 -0500

    metadata generation

--- nft/art_engine/src/generate.py
+++ nft/art_engine/src/generate.py
@@ -1,5 +1,8 @@
 import os
+import io
+import json
 
+ipfs = 'ipfs://QmZQhumc4Kv97LC52Cu6uSyNUsPJa1fets926Msjx3ZNmt'
 output = "output/output"
 layers = [
     "Hat",
@@ -10,7 +13,11 @@ accessoryList = {}
 for layer in layers:
     accessoryList[layer] = ['NONE']
     for acc in os.listdir(f'layers/{layer}'):
-        accessoryList[layer].append(f'layers/{layer}/{acc}')
+        accessoryList[layer].append({
+            'type': layer,
+            'value': acc,
+            'path': f'layers/{layer}/{acc}'
+        })
 
 outputLayers = []  
 
@@ -27,22 +34,43 @@ def permute(idx, curLayer):
             permute(idx+1, curLayer)
             curLayer.pop()
 
-permute(0, [])
+def generateArtwork(outputIdx, outputLayer):
+    # Combine first accessory with base animal
+    os.system(f'convert layers/Animal/corgi.gif -coalesce null:  \( {outputLayer[0]['path']} \
+        -coalesce -delete 4-8 \) \-layers composite -set delay 20 -loop \
+        0 -layers optimize -delete 4-8 {output}{outputIdx}.gif')
+    
+    # Combine remaining
+    i = 1
+    while i < len(outputLayer):
+        os.system(f'convert {output}{outputIdx}.gif -coalesce null:  \( {outputLayer[i]['path']} \
+        -coalesce \) -layers composite -set delay 20 -loop 0 -layers optimize \
+        -delete 4-8 {output}{outputIdx}.gif')
+        i += 1
 
-for outputIdx, outputLayer in enumerate(outputLayers):
-    if len(outputLayer) == 0:
-        os.system(f'cp layers/Animal/corgi.gif output/output{outputIdx}.gif')
-    else:
-        # Combine first accessory with base animal
-        os.system(f'convert layers/Animal/corgi.gif -coalesce null:  \( {outputLayer[0]} \
-            -coalesce -delete 4-8 \) \-layers composite -set delay 20 -loop \
-            0 -layers optimize -delete 4-8 {output}{outputIdx}.gif')
+def generateMetadata(outputIdx, outputLayer):
+    metadata = {
+        'name': f'Pixel Pal #{outputIdx+1}',
+        'description': 'Your digital companion',
+        'image': '{ipfs}/output{outputIdx}.gif',
+        'attributes': []
+    }
+    for layer in outputLayer:
+        metadata['attributes'].append({
+            'trait_type': layer['type'],
+            'value': layer['value']
+        })
         
-        # Combine remaining
-        i = 1
-        while i < len(outputLayer):
-            os.system(f'convert {output}{outputIdx}.gif -coalesce null:  \( {outputLayer[i]} \
-            -coalesce \) -layers composite -set delay 20 -loop 0 -layers optimize \
-            -delete 4-8 {output}{outputIdx}.gif')
-            i += 1
+    with io.open(f'output/json/output{outputIdx}.json', encoding='utf-8') as f:
+        f.write(json.dumps(metadata, ensure_ascii=False))
+
+permute(0, [])
+for outputIdx, outputLayer in enumerate(outputLayers):
+    if len(outputLayer) == 0:   # base animal
+        os.system(f'cp layers/Animal/corgi.gif output/gif/output{outputIdx}.gif')
+        generateMetadata(outputIdx, [])
+    else:
+        generateArtwork(outputIdx, outputLayer)
+        generateMetadata(outputIdx, outputLayer)
+
     
