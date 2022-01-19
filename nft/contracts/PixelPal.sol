2e1a5acf67ee34f1c172b9da661db39fc4762aa3
Michael Jiang <michaelxhj@outlook.com>
2022-01-18 22:20:08 -0500

    upgrades to smart contract to allow update to base uri

--- nft/contracts/PixelPal.sol
+++ nft/contracts/PixelPal.sol
@@ -9,12 +9,20 @@ import "./ERC721Tradable.sol";
  * PixelPal - a contract for my non-fungible PixelPals.
  */
 contract PixelPal is ERC721Tradable {
+    string baseURI = "ipfs://QmUfQX5WiXRf3YiG6raVCXKH9T4zWHP7pqZNPA35ssZbwG/";
+    address constant teamWalletAddress = 0x47FB0BE414D32C5c3d59935b860528Dc88bBec54; //TODO change to team wallet
+
     constructor(address _proxyRegistryAddress)
         ERC721Tradable("PixelPal", "PXP", _proxyRegistryAddress)
     {}
 
-    function baseTokenURI() override public pure returns (string memory) {
-        return "ipfs://QmUfQX5WiXRf3YiG6raVCXKH9T4zWHP7pqZNPA35ssZbwG/";
+    function baseTokenURI() override public view returns (string memory) {
+        return baseURI;
+    }
+
+    function updateBaseURI(string memory newBaseURI) public {
+        assert(teamWalletAddress == msg.sender);
+        baseURI = newBaseURI;
     }
 
     function contractURI() public pure returns (string memory) {
